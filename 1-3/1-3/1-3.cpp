ce60a67b51aac4cbe71c02ce4fec8b3af487bcef
Diana219 <shibzukhovad@list.ru>
2021-06-09 16:12:10 +0300

    Добавлено решение задания 1-3

---
+++ 1-3/1-3/1-3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+/**
+* \brief Вычисление пути
+* \param F - сила тяги
+* \param A - работа
+* \param S - путь
+*/
+
+/**
+* \brief точка входа в программу
+* \return Код ошибки(0 - успех)
+*/
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int F, A, result;
+    cout << "Введите значение силы -";
+    cin >> F;
+    cout << "Введите значение работы -";
+    cin >> A;
+    //S=F*A
+    result = F * A;
+    cout << "S =" << F << "*" << A << "=" << result;
+    cin.get();
+    return 0;
+}
+
